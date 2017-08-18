c666ee9fa4bc276224998b30da4fbaee2dedcee3
dkohlruss <dkohlruss@gmail.com>
2017-08-17 20:22:44 -0600

    Initial commit of redoing the tut

--- server/server.js
+++ server/server.js
@@ -6,18 +6,21 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 let app = express();
-
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('This is working!');
+});
 
 app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
-  })
+  });
 
   todo.save().then((doc) => {
     res.send(doc);
-  }, (err) => {
-    res.status(400).send(err);
+  }, (e) => {
+    res.status(400).send(e);
   });
 });
 
@@ -34,7 +37,7 @@ app.get('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
-
-module.exports = {
-  app
-}
+//
+// module.exports = {
+//   app
+// }
